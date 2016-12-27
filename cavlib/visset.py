37a03b7fa33add89afdaf952d2da6c587be403f5
worron <worrongm@gmail.com>
2016-12-27 04:17:04 +0300

    Gui hadlers fixes

--- cavlib/visset.py
+++ cavlib/visset.py
@@ -15,58 +15,35 @@ class VisualPage(GuiBase):
 		)
 		super().__init__("visset.glade", elements)
 
-		# setup
+		# winstate
 		for prop, value in self.canvas.config["state"].items():
 			self.gui["st_%s_switch" % prop].set_active(value)
+			self.gui["st_%s_switch" % prop].connect("notify::active", self.on_winstate_switch, prop)
 
+		# color
 		self.gui["fg_colorbutton"].set_rgba(self.canvas.config["foreground"])
-		self.gui["bg_colorbutton"].set_rgba(self.canvas.config["background"])
+		self.gui["fg_colorbutton"].connect("color-set", self.on_fg_color_set)
 
+		self.gui["bg_colorbutton"].set_rgba(self.canvas.config["background"])
+		self.gui["bg_colorbutton"].connect("color-set", self.on_bg_color_set)
+
+		# basr
 		self.gui["scale_spinbutton"].set_value(self.canvas.config["scale"])
 		self.gui["padding_spinbutton"].set_value(self.canvas.config["padding"])
-
-		for w in ("top", "bottom", "right", "left"):
-			self.gui[w + "_spinbutton"].set_value(self.canvas.config[w + "_offset"])
-
-		# signals
-		self.gui["st_desktop_switch"].connect("notify::active", self.on_st_desktop_switch)
-		self.gui["st_maximize_switch"].connect("notify::active", self.on_st_maximize_switch)
-		self.gui["st_below_switch"].connect("notify::active", self.on_st_below_switch)
-		self.gui["st_stick_switch"].connect("notify::active", self.on_st_stick_switch)
-		self.gui["st_byscreen_switch"].connect("notify::active", self.on_st_byscreen_switch)
-		self.gui["st_transparent_switch"].connect("notify::active", self.on_st_transparent_switch)
-
-		self.gui["fg_colorbutton"].connect("color-set", self.on_fg_color_set)
-		self.gui["bg_colorbutton"].connect("color-set", self.on_bg_color_set)
 
 		self.gui["scale_spinbutton"].connect("value-changed", self.on_scale_spinbutton_changed)
 		self.gui["padding_spinbutton"].connect("value-changed", self.on_padding_spinbutton_changed)
 
+		# offset
 		for w in ("top", "bottom", "right", "left"):
-			self.gui[w + "_spinbutton"].connect("value-changed", self.on_offset_spinbutton_changed)
+			self.gui[w + "_spinbutton"].set_value(self.canvas.config[w + "_offset"])
+			self.gui[w + "_spinbutton"].connect("value-changed", self.on_offset_spinbutton_changed, w)
 
-	# window state
-	# TODO(?): universal handler
-	def on_st_desktop_switch(self, switch, *args):
-		self.canvas.desktop = switch.get_active()
-		self.canvas._rebuild_window()
+	def on_winstate_switch(self, switch, active, key):
+		setattr(self.canvas, key, switch.get_active())
+		if key == "desktop":
+			self.canvas._rebuild_window()
 
-	def on_st_maximize_switch(self, switch, *args):
-		self.canvas.maximize = switch.get_active()
-
-	def on_st_below_switch(self, switch, *args):
-		self.canvas.below = switch.get_active()
-
-	def on_st_stick_switch(self, switch, *args):
-		self.canvas.stick = switch.get_active()
-
-	def on_st_byscreen_switch(self, switch, *args):
-		self.canvas.byscreen = switch.get_active()
-
-	def on_st_transparent_switch(self, switch, *args):
-		self.canvas.transparent = switch.get_active()
-
-	# colors
 	def on_fg_color_set(self, button):
 		self.canvas.config["foreground"] = button.get_rgba()
 
@@ -84,9 +61,6 @@ class VisualPage(GuiBase):
 		self.canvas.config["padding"] = int(button.get_value())
 		self.canvas.draw.size_update()
 
-	def on_offset_spinbutton_changed(self, button):
-		for w in ("top", "bottom", "right", "left"):
-			if self.gui[w + "_spinbutton"] == button:
-				self.canvas.config[w + "_offset"] = int(button.get_value())
-				self.canvas.draw.size_update()
-				break
+	def on_offset_spinbutton_changed(self, button, key):
+		self.canvas.config[key + "_offset"] = int(button.get_value())
+		self.canvas.draw.size_update()
