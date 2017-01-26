88490fb17ddd263bd875106f7fce8eaaebef0654
worron <worrongm@gmail.com>
2017-01-27 01:37:06 +0300

    Setup script fixes

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ $ cat log.txt | xargs rm -rf
 from setuptools import setup
 
 setup(
-	name="cavalcade",
-	version="0.6.dev1",
-	description="GUI wrapper for C.A.V.A. utility",
+	name = "cavalcade",
+	version = "0.6.dev1",
+	description = "GUI wrapper for C.A.V.A. utility",
 	LICENSE = "GPL",
-	author="worron",
-	author_email="worrongm@gmail.com",
-	url="https://github.com/worron/cavalcade",
-	packages=["cavalcade", 'cavalcade.gui', 'cavalcade.data'],
+	author = "worron",
+	author_email = "worrongm@gmail.com",
+	url = "https://github.com/worron/cavalcade",
+	packages = ["cavalcade", "cavalcade.gui", "cavalcade.data"],
 	install_requires = ["setuptools"],
 	package_data = {"cavalcade.gui": ["*.glade"], "cavalcade.data": ["*.ini", "*.svg"]},
 	entry_points = {
