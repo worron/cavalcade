7cea23001ab2c42a74a7f8ac63383b3e3d63df69
worron <worrongm@gmail.com>
2017-02-01 22:39:18 +0300

    New GUI fixes

--- cavalcade/visset.py
+++ cavalcade/visset.py
@@ -13,10 +13,9 @@ class VisualPage(GuiBase):
 		self.settings = settings
 
 		elements = (
-			"fg_colorbutton", "zero_spinbutton", "silence_spinbutton", "image_open_button",
-			"bg_colorbutton", "padding_spinbutton", "scale_spinbutton", "top_spinbutton", "bottom_spinbutton",
-			"left_spinbutton", "right_spinbutton", "image_tag_rbutton", "imagelabel",
-			"image_file_rbutton", "mainbox", "offset-comboboxtext", "offset-spinbutton",
+			"fg-colorbutton", "zero-spinbutton", "silence-spinbutton", "image-open-button",
+			"bg-colorbutton", "padding-spinbutton", "scale-spinbutton", "image-tag-radiobutton", "image-label",
+			"image-file-radiobutton", "mainbox", "offset-comboboxtext", "offset-spinbutton",
 		)
 		super().__init__("visset.glade", elements=elements)
 
@@ -27,13 +26,18 @@ class VisualPage(GuiBase):
 
 		# color
 		for key in ("fg", "bg"):
-			self.gui["%s_colorbutton" % key].set_rgba(self.config["color"][key])
-			self.gui["%s_colorbutton" % key].connect("color-set", getattr(self, "on_%s_color_set" % key))
+			self.gui["%s-colorbutton" % key].set_rgba(self.config["color"][key])
+			self.gui["%s-colorbutton" % key].connect("color-set", getattr(self, "on_%s_color_set" % key))
 
 		# graph
+		self.gui["scale-spinbutton"].set_adjustment(Gtk.Adjustment(1, 0.5, 5, 0.1, 0, 0))
+		self.gui["zero-spinbutton"].set_adjustment(Gtk.Adjustment(1, 0, 50, 1, 0, 0))
+		self.gui["silence-spinbutton"].set_adjustment(Gtk.Adjustment(10, 1, 60, 1, 0, 0))
+		self.gui["padding-spinbutton"].set_adjustment(Gtk.Adjustment(5, 1, 50, 1, 0, 0))
+
 		for key, value in self.config["draw"].items():
-			self.gui["%s_spinbutton" % key].set_value(value)
-			self.gui["%s_spinbutton" % key].connect("value-changed", getattr(self, "on_%s_spinbutton_changed" % key))
+			self.gui["%s-spinbutton" % key].set_value(value)
+			self.gui["%s-spinbutton" % key].connect("value-changed", self.on_draw_spinbutton_changed, key)
 
 		# offset
 		self.offset_current = None
@@ -47,13 +51,14 @@ class VisualPage(GuiBase):
 		self.gui["offset-spinbutton"].connect("value-changed", self.on_offset_spinbutton_changed)
 
 		# image source
-		image_rb = "image_tag_rbutton" if self.config["image"]["usetag"] else "image_file_rbutton"
+		image_rb = "image-tag-radiobutton" if self.config["image"]["usetag"] else "image-file-radiobutton"
 		self.gui[image_rb].set_active(True)
 
-		self.gui["image_tag_rbutton"].connect("notify::active", self.on_image_rbutton_switch, True)
-		self.gui["image_file_rbutton"].connect("notify::active", self.on_image_rbutton_switch, False)
+		self.gui["image-tag-radiobutton"].connect("notify::active", self.on_image_rbutton_switch, True)
+		self.gui["image-file-radiobutton"].connect("notify::active", self.on_image_rbutton_switch, False)
 
-		self.gui["image_open_button"].connect("clicked", self.on_image_open_button_click)
+		self.gui["image-open-button"].connect("clicked", self.on_image_open_button_click)
+		self.gui["image-label"].set_text("Image: %s" % name_from_file(self.config["image"]["default"]))
 
 		# misc
 		self._mainapp.connect("ac-update", self.on_autocolor_update)
@@ -73,7 +78,7 @@ class VisualPage(GuiBase):
 		self.config["color"]["auto"] = autocolor
 
 		color = self.config["color"]["autofg"] if autocolor else self.config["color"]["fg"]
-		self.gui["fg_colorbutton"].set_rgba(color)
+		self.gui["fg-colorbutton"].set_rgba(color)
 		self._mainapp.draw.color_update()
 
 	# signal handlers
@@ -90,22 +95,14 @@ class VisualPage(GuiBase):
 	def on_autocolor_update(self, sender, rgba):
 		self.config["color"]["autofg"] = rgba
 		if self.config["color"]["auto"]:
-			self.gui["fg_colorbutton"].set_rgba(rgba)
+			self.gui["fg-colorbutton"].set_rgba(rgba)
 			self._mainapp.draw.color_update()
 
-	def on_scale_spinbutton_changed(self, button):
-		self.config["draw"]["scale"] = float(button.get_value())
-
-	def on_padding_spinbutton_changed(self, button):
-		self.config["draw"]["padding"] = int(button.get_value())
-		self._mainapp.draw.size_update()
-
-	def on_zero_spinbutton_changed(self, button):
-		self.config["draw"]["zero"] = int(button.get_value())
-		self._mainapp.draw.size_update()
-
-	def on_silence_spinbutton_changed(self, button):
-		self.config["draw"]["silence"] = int(button.get_value())
+	def on_draw_spinbutton_changed(self, button, key):
+		type_ = float if key == "scale" else int
+		self.config["draw"][key] = type_(button.get_value())
+		if key in ("padding", "zero"):
+			self._mainapp.draw.size_update()
 
 	def on_offset_spinbutton_changed(self, button):
 		self.config["offset"][self.offset_current] = int(button.get_value())
@@ -126,6 +123,6 @@ class VisualPage(GuiBase):
 	def on_image_open_button_click(self, *args):
 		is_ok, file_ = gtk_open_file(self.window, self.image_filter)
 		if is_ok:
-			self.gui["imagelabel"].set_text("Image: %s" % name_from_file(file_))
+			self.gui["image-label"].set_text("Image: %s" % name_from_file(file_))
 			self.config["image"]["default"] = file_
 			self._mainapp.emit("default-image-update", file_)
